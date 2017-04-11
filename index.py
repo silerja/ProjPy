4d94338814ad5590ec80f577fcc5f6282c6179e3
Joseph Siler <Joseph Siler>
2017-04-11 13:29:16 -0400

    first question

---
+++ index.py
@@ -0,0 +1,15 @@
+def quiz():
+    print("Here is a quiz to test your knowledge of your classmates")
+    print()
+    print()
+    print("Question 1")
+    print("Who do you think this is?")
+    print()
+    print("a. Joe")
+    print("b. Cordell")
+    print("c. Scott")
+    answer = input("Make your choice: ")
+    if answer == "a":
+        print("Correct!")
+    if answer =="b","c":
+        print("Incorrect!")
